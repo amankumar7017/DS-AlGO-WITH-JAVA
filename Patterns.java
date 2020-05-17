ec3fa769e8a668352ecba76e47276dff34c19e38
AMAN <40682420+Duchies@users.noreply.github.com>
2020-05-17 20:46:32 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -1,6 +1,5 @@
 
 public class Patterns{
-//digit of a number;
     public static void main(String[] args) {
                
        //pattern program
@@ -228,7 +227,15 @@ public class Patterns{
         }
         System.out.println("");
     }
-
+// output
+// 1 
+// 2 3 
+// 5 8 13 
+// 21 34 55 89 
+// 144 233 377 610 987 
+// 1597 2584 4181 6765 10946 17711 
+        
+        
 } 
 }
     
