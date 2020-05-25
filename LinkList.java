ac3f42de5dc18535bf4b66dd256344a98e455b89
AMAN <40682420+Duchies@users.noreply.github.com>
2020-05-25 11:17:16 +0530

    Add files via upload

---
+++ LinkList.java
@@ -0,0 +1,180 @@
+import java.util.*;
+public class LinkList {
+    
+    //Single Linkedlist operatios(insert,delete,display)
+  
+  public class Node{
+        int data;
+        Node next;
+        
+        Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+        Node root = null;// its a head of node;
+        Node tail = null;// its a tail of node;
+   public void insertAtLast(int data){
+       Node node = new Node(data);
+   
+       if(root == null){
+        root = node;
+        tail = node;
+    }else{
+          
+          tail.next = node;  
+          tail = node;
+          
+       }
+       
+   }
+   public void insertAtBeg(int data){
+       Node node = new Node(data);
+       
+       if(root == null){
+           root =  node;
+           tail = node;
+       }else{
+           Node temp = root;
+           
+           root = node;
+           node.next  = temp;
+           
+       }
+           
+       
+       
+   }
+   public void insertAt(int data,int loc){
+       Node node = new Node(data);
+       if(root == null){
+           root = node;
+           tail = node;
+       }else{
+           int s= size();
+           if(loc>s){
+               System.out.println("invalid location data can't be inserted");
+           }else{
+               
+               Node temp = root;
+               int i=1; 
+              
+               while(i < loc){
+                   temp = temp.next;
+                   i++;
+               }
+             
+               Node temp2 = temp.next;
+               temp.next = node;
+               node.next = temp2;
+               System.out.println("insertion Sucessfull");
+           
+           }
+        
+       }
+       
+   }
+    
+   public void deleteAtBeg(){
+       if(root!=null){
+       Node temp = root ;
+        System.out.println("\ndeletion at begninig Sucessfull\ndeleted data "+temp.data);
+       temp = temp.next;
+       root = temp;
+      
+       }else{
+           System.out.println("root is already empty deletion not possible");
+       }
+   }
+   public void deleteAtLast(){
+       if(root== null){
+           System.out.println("No element  present in linked list");
+       }else{
+           Node temp = root;
+           while(temp.next.next!=null){
+               temp = temp.next;
+           }
+           
+           System.out.println("deletion At last sucessfull\ndeleted data "+temp.data);
+           temp.next = null;
+       }
+   }
+   
+   public void deleteAt(int loc){
+       if(root == null && loc > size()){
+           System.out.println("location invalid or may be no element in Linked List");
+       }else{
+           Node temp = root;
+           int s= size();
+           int i=1;
+       /* this approch don't work with some corner cases
+            while(i < loc){
+                temp = temp.next;
+                i++;
+            }          
+           temp.next = temp.next.next;
+            */
+       Node prev = null;
+       
+       while(i < loc){
+           prev=temp;
+           temp = temp.next;
+           i++;
+       }
+           System.out.println("\ndeletion at specific location "+loc+" sucessfull\ndeleted data "+temp.data);
+        prev.next  = temp.next;
+           
+       }
+   }
+   
+    public int size(){
+        if(root == null){
+            return 0;
+        }
+        Node temp = root;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+   
+     public void display(){
+         System.out.println("Linked list");
+         if(root == null){
+             System.out.println("No element in Linked list");
+         }else{
+             Node temp = root;
+ 
+             while(temp !=null){
+                 System.out.print(temp.data+" ");
+                 temp = temp.next;
+     
+             }
+         
+        }
+     }   
+    
+    public static void main(String[] args) {
+        
+        LinkList obj = new LinkList();
+        obj.insertAtLast(10);
+        obj.insertAtLast(12);
+        obj.insertAtLast(14);
+        obj.insertAtLast(16);
+         obj.insertAtBeg(9);
+          obj.insertAtBeg(8);
+          obj.insertAtBeg(2);
+          obj.insertAtBeg(5);
+         
+         obj.insertAt(99,3);
+          obj.display(); 
+          obj.deleteAtBeg();
+          obj.deleteAtLast();
+          obj.display();
+          obj.deleteAt(2);
+          obj.display();
+   
+        }
+    }
