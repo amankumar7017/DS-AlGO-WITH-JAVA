766bc9e7c4c5e2ef0d5f680e7666add92e61c3c1
AMAN <40682420+Duchies@users.noreply.github.com>
2020-06-21 20:03:38 +0530

    Update DublicateFindinArray.java

--- Arrays/DublicateFindinArray.java
+++ Arrays/DublicateFindinArray.java
@@ -1,6 +1,13 @@
 import java.util.*;
 class DublicateFindinArray{
+		
 	
+	//main task  to find in O(1) without any extra space..
+	
+	// Assumption for input array number is less or equal to  array length 
+	
+	//main crux  = make all number negetive by taking reference of each element in array . a number will be detectected 
+	//             for dublicate that , that number is already has negetive index .. 
 
 	public static void main(String[] args) {
 			
