36d04d243ff4ba07d10d13bc084cf51ce40dfcfd
AMAN <40682420+Duchies@users.noreply.github.com>
2020-07-29 20:29:51 +0530

    using inOrder and preOrder traversal constructing Tree

---
+++ Link List/InOrderPreOrderTree.java
@@ -0,0 +1,70 @@
+class InOrderPreOrderTree{
+
+	
+	class Node{
+		int data ;
+		Node left;
+		Node right ;
+
+
+	}
+
+
+	static int[] in = {40,20,50,10,30};
+	static int[] pre = {10,20,40,50,30};
+
+
+	public void constuct(){
+		Node n = this.constuct(0,pre.length-1,0,in.length-1);
+
+		display(n);
+	} 
+
+	private Node constuct(int preLow,int preHi,int inLow,int inHi){
+
+		if(inLow > inHi || preLow > preHi){
+			return null;
+		}
+
+		Node nn = new Node();
+		nn.data = pre[preLow];
+
+		int searchIndex = -1;
+		int numberOfElement=0;
+
+		for(int i=0;i<inHi;i++){
+			if(pre[preLow] == in[i]){
+				searchIndex = i;
+			}
+			numberOfElement++;
+		}	
+
+		nn.left = constuct(preLow+1,preLow+numberOfElement, inLow,searchIndex-1);
+		nn.right = constuct(preLow+numberOfElement+1,preHi,searchIndex+1,inHi);
+		return  nn;
+	}
+
+	public void display(Node node){
+
+		if(node == null){
+			return ;
+		}
+
+			display(node.left);
+			System.out.println(node.data);
+			display(node.right);
+			
+
+		
+	}
+
+public static void main(String[] args) {
+	
+
+	InOrderPreOrderTree tree = new InOrderPreOrderTree();
+	tree.constuct();
+
+}
+
+
+}
